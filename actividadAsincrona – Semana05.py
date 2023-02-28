59f095ebfb4590213f2ac605b1ec5355f40a0d74
wendy97531234 <126539753+wendy97531234@users.noreply.github.com>
2023-02-27 21:45:24 -0600

    Add files via upload

---
+++ actividadAsincrona – Semana05.py
@@ -0,0 +1,108 @@
+print("bien venido anuestro primer programa de fundamentos de programacion")
+print("acontinuacion se presentaran los datos de cada estudiante los cuales serian sus nombres,apellidos,sexo y edad ") 
+print("integrante numero 1")
+nombre = "Nombres: Bryan Alexander"
+print(nombre)
+apellidos = "Apellidos: Pineda Chavez"
+print(apellidos)
+sexo = "Sexo: M"
+print(sexo)
+edad = "Edad: 19"
+print(edad)
+print("Integrante numero 2")
+nombre_2 = "Nombres: Xiomara Abigail"
+print(nombre_2)
+apellidos_2 = ("Apellidos: Cisneros Peña")
+print(apellidos_2)
+sexo_2 = "sexo: F"
+print(sexo_2)
+edad_2 = ("edad: 18")
+print(edad_2)
+print("Integrante numero 3")
+nombre_3 = "Nombres: Jesus Rodrigo"
+print(nombre_3)
+apellidos_3 = ("Apellidos: Landaverde Alvarado")
+print(apellidos_3)
+sexo_3 = "sexo: M"
+print(sexo_3)
+edad_3 = ("edad: 19")
+print(edad_3)
+print("Integrante numero 4")
+nombre_4 = "Nombres: Wendy Daniela"
+print(nombre_4)
+apellidos_4 = ("Apellidos: Ayala Ayala")
+print(apellidos_4)
+sexo_4 = "sexo: F"
+print(sexo_4)
+edad__4 = ("edad: 18")
+print(edad__4)
+print("Integrante numero 5")
+nombre_5 = "Nombres: Eduardo Nathaniel"
+print(nombre_5)
+apellidos_5 = ("Apellidos: Sales Arteaga")
+print(apellidos_5)
+sexo_5 = "sexo: M"
+print(sexo_5)
+edad_5 = ("edad: 18")
+print(edad_5)
+print("Integrante numero 6")
+nombre_6 = "Nombres: Luis Miguel"
+print(nombre_6)
+apellidos_6 = ("Apellidos: Abrego Sanchez")
+print(apellidos_6)
+sexo_6 = "sexo: M"
+print(sexo_6)
+edad_6 = ("edad: 18")
+print(edad_6)
+
+print("Bryan" in nombre)
+
+n = int(input("ingresa el numero de alumnos: "))
+x = 1
+suma = 0
+while x <= n:
+    edad = int(input("ingresa una edd: "))
+    suma = suma + edad
+    x += 1
+    print("el promedio de edades es: ",suma / n )
+    nombre = "bryan Alexander"
+apellidos = "pineda chavez"
+
+nombreapellido = nombre + ", " + apellidos 
+
+print(nombreapellido)
+   
+nombre_2 = "Xiomara Abigail"
+apellidos_2 = "Cisneros Peña"
+
+nombre_2apellido_2 = nombre_2 + ", " + apellidos_2 
+
+print(nombre_2apellido_2)
+
+nombre_3 = "Jesus Rodrigo"
+apellidos_3 = "Landaverde Alvarado"
+
+nombre_3apellidos_3 = nombre_3 + ", " + apellidos_3 
+
+print(nombre_3apellidos_3)
+
+nombre_4 = "Wendy Daniela"
+apellidos_4 = "Ayala Ayala"
+
+nombre_4apellido_4 = nombre_4 + ", " + apellidos_4 
+
+print(nombre_4apellido_4)
+
+nombre_5 = "Luis Miguel"
+apellidos_5 = "Sales Arteaga"
+
+nombre_5apellido_5 = nombre_5 + ", " + apellidos_5 
+
+print(nombre_5apellido_5)
+
+nombre_6 = "Eduardo Nathaniel"
+apellidos_6 = "Sales Arteaga"
+
+nombre_6apellidos_6 = nombre_6 + ", " + apellidos_6
+
+print(nombre_6apellidos_6)
